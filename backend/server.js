685d6e3bf30a085d02e7f76dc032b90a54c62d7a
Josy George <joe.george.josy@gmail.com>
2022-09-11 21:23:53 -0400

    express server initial setup

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
