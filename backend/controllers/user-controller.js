e0dc1930c049af8768ca9a04aed15138911a1e73
Josy George <joe.george.josy@gmail.com>
2022-09-14 14:55:11 -0400

    user logic correction

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,36 +1,37 @@
 const User = require('../models/users');
 
-const { hashPassword, comparePassword } = require('../utils/auth');
+const { hashPassword } = require('../utils/auth');
 
 const register = async (req, res) => {
     try {
-        console.log(req.body)
+        console.log(req.body);
         const { firstname, lastname, email, password } = req.body;
         // Validation
-        if (!firstname) return res.status(400).send('First Name is required!')
-        if (!lastname) return res.status(400).send('Last Name is required!')
+        if (!firstname) return res.status(400).send('First Name is required!');
+        if (!lastname) return res.status(400).send('Last Name is required!');
+        if (!email) return res.status(400).send('Email is required!');
         if (!password || password.length < 6) {
-            return res.status(400).send('Password should be min 6 characters in length')
+            return res.status(400).send('Password should be min 6 characters in length');
         }
         // Mongoose will not execute a query until then or exec has been called upon it.
-        let userExists = await User.findOne({ email }).exec()
-        if (userExists) return res.status(400).send('User / Email already exists!')
+        let userExists = await User.findOne({ email }).exec();
+        if (userExists) return res.status(400).send('User / Email already exists!');
         // Assigning Hashed Password calling function
-        const hashedPassword = await hashPassword(password)
-        console.log(firstname)
+        const hashedPassword = await hashPassword(password);
+        console.log(firstname);
         // Register User
         const user = new User({
-            firstname: firstname,
-            lastname: lastname,
+            firstname,
+            lastname,
             email,
             password: hashedPassword
         })
         await user.save();
-        console.log("Saved User", user)
-        return res.json({ ok: true })
+        console.log("Saved User", user);
+        return res.json({ ok: true });
     } catch (err) {
-        console.log(err)
-        return res.status(400).send('Error! Please try again.')
+        console.log(err);
+        return res.status(400).send('Error! Please try again.');
     }
 };
 
