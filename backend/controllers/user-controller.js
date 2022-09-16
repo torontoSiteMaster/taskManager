d19155dfef1461dc5eecb16024c4c5e2c6ea7ca2
Josy George <joe.george.josy@gmail.com>
2022-09-16 10:11:43 -0400

    user logout

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -78,4 +78,14 @@ const login = async (req, res) => {
     }
 };
 
-module.exports = { register, login }; 
+const logout = async (req, res) => {
+    try {
+        res.clearCookie("token")
+        return res.json({ message: "User logged out!" })
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = { register, login, logout }; 
--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 // controllers
-const { register, login } = require('../controllers/user-controller');
+const { register, login, logout } = require('../controllers/user-controller');
 
 // Router
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
 
 module.exports = router;
