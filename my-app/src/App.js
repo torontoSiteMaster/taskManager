5dc994f4d6ede2401d095983eb01952011016d95
abdalbagi <aabdalbagi93@gmail.com>
2022-09-22 20:40:53 -0400

    login-signup form

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/login';
+import Signup from './components/signup'
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Signup /> */}
+     <Login />
+    </div>
+  );
+}
+
+export default App;
